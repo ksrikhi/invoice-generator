b1073914ed10bd9647582be8e137636a156c3c9d
kanchan sharma <ksrikhi123@gmail.com>
2020-10-17 04:23:31 +0530

    testing

--- controllers/application.js
+++ controllers/application.js
@@ -1,11 +1,11 @@
 const pdf = require('html-pdf');
-const data = require('../mock/data.json');
+// const data = require('../mock/data.json');
 const getPdfTemplates = require('../helper/pdfTemplate');
 const sendEmail = require('../helper/sendEmail');
 
 const generatePdfAndSendEmail = (req, res, next) => {
-  // const { body } = req;
-  // const data = body;
+  const { body } = req;
+  const data = body;
   const html = getPdfTemplates(data);
   const options = { 
   "format": 'Letter',
@@ -20,11 +20,10 @@ const generatePdfAndSendEmail = (req, res, next) => {
   fileName = `../tmp/invoice${Math.ceil(Math.random(1)*100000)}.pdf`;
   pdf.create(html, options).toFile(fileName, function (err, res) {
     if (err) return console.log(err);
-    // sendEmail(data, res.filename);
+    sendEmail(data, res.filename);
     console.log(res)
   });
 }
-generatePdfAndSendEmail()
 
 
 module.exports = generatePdfAndSendEmail
