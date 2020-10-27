4e8ddb91570f68a63ff2ae6796fe429b91eef051
kanchan sharma <ksrikhi123@gmail.com>
2020-10-28 01:10:06 +0530

    add style in pdf

--- helper/pdfTemplate.js
+++ helper/pdfTemplate.js
@@ -35,87 +35,7 @@ const getPdfTemplates = (data) => {
     <title>INVOICE</title>
     <style>
 
-    .invoice-box {
-        max-width: 800px;
-        margin: auto;
-        padding: 30px;
-        border: 1px solid #eee;
-        box-shadow: 0 0 10px rgba(0, 0, 0, .15);
-        font-size: 16px;
-        line-height: 24px;
-        font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
-        color: #555;
-    }
     
-    .invoice-box table {
-        width: 100%;
-        line-height: inherit;
-        text-align: left;
-    }
-    
-    .invoice-box table td {
-        padding: 5px;
-        vertical-align: top;
-    }
-    
-    .invoice-box table tr td:nth-child(2) {
-        text-align: right;
-    }
-    
-    .invoice-box table tr.top table td {
-        padding-bottom: 20px;
-    }
-    
-    .invoice-box table tr.top table td.title {
-        font-size: 45px;
-        line-height: 45px;
-        color: #333;
-    }
-    
-    .invoice-box table tr.information table td {
-        padding-bottom: 40px;
-    }
-    
-    .invoice-box table tr.heading td {
-        background: #eee;
-        border-bottom: 1px solid #ddd;
-        font-weight: bold;
-    }
-    
-    .invoice-box table tr.details td {
-        padding-bottom: 20px;
-    }
-    
-    .invoice-box table tr.item td{
-        border-bottom: 1px solid #eee;
-    }
-    
-    .invoice-box table tr.item.last td {
-        border-bottom: none;
-    }
-    
-    .invoice-box table tr.total td:nth-child(2) {
-        border-top: 2px solid #eee;
-        font-weight: bold;
-    }
-    
-    @media only screen and (max-width: 600px) {
-        .invoice-box table tr.top table td {
-            width: 100%;
-            display: block;
-            text-align: center;
-        }
-        
-        .invoice-box table tr.information table td {
-            width: 100%;
-            display: block;
-            text-align: center;
-        }
-    }
-    
-
-
-
     @page {
         size: A4;
     }
@@ -136,13 +56,14 @@ const getPdfTemplates = (data) => {
         color: #2a2a2a;
         font-family: Helvetica, Arial, sans-serif;
         max-width: 1200px;
-        width: 100%;
         margin-top: 1em;
         box-sizing: border-box;
         font-size:16px;
     }
 
         .flex-container {
+            max-width: 800px;
+            min-width: 800px;
             margin-left: 10%;
             margin-right: 10%;
             margin-top: 1em;
@@ -178,16 +99,15 @@ const getPdfTemplates = (data) => {
             justify-content: flex-start;
         }
 
-        table,
-        th,
-        td {
+        table.item,
+        th.item,
+        td.item {
             border: 1px solid black;
             border-collapse: collapse;
             margin: 25px 0 10px;
             text-align: center;
             table-layout: fixed;
             width:100%;
-           
         }
 
         td:empty::after {
@@ -214,10 +134,8 @@ const getPdfTemplates = (data) => {
         </div>
         <div style="border-top: 5px solid gray; margin: 30px 0;"></div>
 
-
-
         <div  style='padding: 10px 0; display: table;'>
-            <div style='display: table-cell; vertical-align: top; width:70%;'>
+            <div style='display: table-cell; vertical-align: top; width:40%'>
                 <p> <b>Billing To:</b> </P>
                 <p>${clientName}</p>
                 ${streetAddress ? `<p>${streetAddress}</p>` : ''}
@@ -225,50 +143,65 @@ const getPdfTemplates = (data) => {
                 <p>${email}</p>
             </div>
             <div style='display: table; overflow: hidden; margin: 0 0 10px;'>
-                    <div style='display: table-cell; vertical-align: top;text-align: left; padding-right: 5px; margin-left: 90px;'>
-                        <p> <b>Issued Date: </b></p>
-                        <p> <b>Invoice No: </b> </p>
-                    </div>
-                    <div style="vertical-align: top; display: table-cell;">
-                        <p>${currentDate}</p>
-                        ${invoiceNumber ? `<p>${invoiceNumber}</p>` : ''}
-                    </div>
+
+        <table style=' margin-left:270px;'>
+        <tr >
+            <td>
+                <p> <b>Issued Date: </b></p>
+                <p> <b>Invoice No: </b> </p>
+            </td>
+            
+            <td>
+            <p>${currentDate}</p>
+            ${invoiceNumber ? `<p>${invoiceNumber}</p>` : ''}
+            </td>
+        </tr>
+    </table>
             </div>
         </div>
 
 
-        <table >
-            <tr>
-                <th>Discription</th>
-                <th>Unit Cost</th>
-                <th>Quantity</th>
-                <th>Amount</th>
+        <table class="item" >
+            <tr class="item" >
+                <th class="item" >Discription</th>
+                <th class="item" >Unit Cost</th>
+                <th class="item" >Quantity</th>
+                <th class="item" >Amount</th>
             </tr>
             ${items.map(({ discription, unitCost, quantity } = item) => {
         return `
-                <tr>
-                <td >${discription}</td>
-                <td>$${unitCost}</td>
-                <td>${quantity}</td>
-                <td>$${(+quantity * +unitCost).toFixed(2)}</td>
+                <tr class="item" >
+                <td class="item">${discription}</td>
+                <td class="item">$${unitCost}</td>
+                <td class="item" >${quantity}</td>
+                <td class="item" >$${(+quantity * +unitCost).toFixed(2)}</td>
             </tr>
            `
     })}
         </table>
 
 
-        <div style="flex: 100%; margin-left: 40%;">
-            <p>Subtotal:<span> $${subtotal}</span> </p>
-            <p>Tax Rate:<span>13%</span></p>
-            <p>Tax Amount:<span style=" margin-left: 38%; ">${taxFormated}</span></p>
+        <div style="flex: 100%; margin-left: 50%;">
+            <p>Subtotal:&nbsp;&nbsp;&nbsp;<span> $${subtotal}</span> </p>
+            <p>Tax Rate:&nbsp;&nbsp;&nbsp;<span>13%</span></p>
+            <p>Tax Amount:<span style="margin-left: 38%; ">$${taxFormated}</span></p>
         </div>
-        <div style="flex: 100%; margin-left: 35%;border-top: 5px solid gray;"></div>
-            <p style="flex: 100% ;  margin-left: 300px;"><b>Total:</b><span><b>$${total.toFixed(2)}</b></span></p>
+        <div style="flex: 100%; margin-left: 42%;border-top: 5px solid gray;"></div>
+
+        <table style=' margin-left:50%;'>
+        <tr >
+            <td>
+            <p style="flex: 100%"><b>Total:</b><span style='margin-left:98%';><b>$${total.toFixed(2)}</b></span></p>
         
-        <div style="flex: 100%; margin-left:35%;border-top: 5px solid gray;"></div>
+            </td>
+        </tr>
+    </table>
+
+            
+        <div style="flex: 100%; margin-left:42%;border-top: 5px solid gray;"></div>
         <div>
           <p style="margin-bottom: 30px; margin-top: 20px;  text-align: center; font-weight: 200;font-size: 17px;"><b>Thank you for your business!</b></p>
-            <p style= "text-align: center;font-weight: 200;font-size: 17px;">if you have any questions or inquaries, please contect.</p>
+            <p style= "text-align: center;font-weight: 200;font-size: 17px; margin-left: 10px">if you have any questions or inquaries, please contect.</p>
             <p style="text-align: center;font-weight: 200;font-size: 17px;">Contect Name,${mobileNumber}, or ${emailaddress}</p>
         </div>
     </div>
